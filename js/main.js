f7d146d6f1a84bfd44f214193655022d1282a4f6
edserna <ed.jserna@gmail.com>
2019-10-10 13:19:58 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let cantidadDeDolares = 500
+
+let tasaDeCambio = 19
+
+let cantidadDePesosAhorrar = cantidadDeDolares * tasaDeCambio
+
+console.log('La cantidad que fulanito tiene que ahorrar es:',cantidadDePesosAhorrar)
